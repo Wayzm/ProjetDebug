9e3d459443acf13db4c89b029bec3e36d67a7ace
Wayzm <wyeumo@yahoo.fr>
2022-02-08 16:28:19 +0100

    --- WIP ---

--- include/tools.h
+++ include/tools.h
@@ -1,6 +1,14 @@
 // .h
+#ifndef TOOLS_H
+#define TOOLS_H
 
+void choice ();
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
+void tracer(pid_t Child);
+
+void code (char **argc);
+
+void segferror (int sig, siginfo_t *info, void *ctx);
+
+void fperror (int sig, siginfo_t *info, void *ctx);
+#endif
