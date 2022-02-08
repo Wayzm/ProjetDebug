42a272310cbdd0a9e9984974815411b3642bcce3
Wayzm <wyeumo@yahoo.fr>
2022-02-08 16:28:37 +0100

    --- Cleaned main a bit ---

--- src/main.c
+++ src/main.c
@@ -6,33 +6,11 @@
 
 #include "tools.h"
 
-int* vector;
-
-char key;
-
-void choice (int nbr, char **argc)
-{
-    key = 0;
-
-    switch(key){
-      case 's':
-        // Segfaul
-        break;
-      case 'c':
-        // catch SIGINT
-        break;
-      case 'q':
-        break;
-      default:
-        printf("How to use : \n \n Press s to find the segfault. \n Press c to catch SIGINT. \n Press q to exit.");
-      break;
-    }
-}
-
 
 int main (int argv, char ** argc)
 {
-  char key;
+
+  choice();
   printf("LOL\n");
   return 0;
 }
